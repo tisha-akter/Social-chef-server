9b0945a59b37170220cfa3a9466ca70f0a2ed4fd
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-03 21:31:51 +0600

    added json for view recipe

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const cards = require('./data/cards.json');
+const banner = require('./data/recipe-banner.json');
 
 app.get('/', (req, res) =>{
     res.send('Chef is runnig')
@@ -14,7 +15,19 @@ app.get('/', (req, res) =>{
 
 app.get('/cards', (req, res) => {
     res.send(cards);
-}) 
+});
+
+app.get('/banner', (req, res) => {
+    res.send(banner);
+})
+
+app.get('/banner/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedBanner = banner?.find(n=> n.id == id);
+    res.send(selectedBanner)
+})
+
 
 app.listen(port, () => {
     console.log(`Chef is running on port: ${port}`)
